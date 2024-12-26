70d704af83ca5e45c7c7247f18bb8d0d029f3162
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-26 15:33:52 -0500

    feat(cycles): add general Cycles class

---
+++ src/cycles/Cycles.ts
@@ -0,0 +1,17 @@
+import { hexaCycles, octaCycles, enneaCycles } from "./hexa-octa-enea-cycles";
+import { hamiltonianCycle1, hamiltonianCycle2, hamiltonianCycle3, hamiltonianCycle4, hamiltonianCycle5, hamiltonianCycle6, hamiltonianCycle7, hamiltonianCycle8 } from "./hamiltonian-cycles";
+
+export class Cycles {
+  static hexaCycles = hexaCycles;
+  static octaCycles = octaCycles;
+  static enneaCycles = enneaCycles;
+
+  static hamiltonianCycle1 = hamiltonianCycle1;
+  static hamiltonianCycle2 = hamiltonianCycle2;
+  static hamiltonianCycle3 = hamiltonianCycle3;
+  static hamiltonianCycle4 = hamiltonianCycle4;
+  static hamiltonianCycle5 = hamiltonianCycle5;
+  static hamiltonianCycle6 = hamiltonianCycle6;
+  static hamiltonianCycle7 = hamiltonianCycle7;
+  static hamiltonianCycle8 = hamiltonianCycle8;
+}
