78fe8db317841dcae2213c88ad868407fa533418
Edgar Delgado Vega <edelve91@gmail.com>
2024-12-26 15:34:26 -0500

    feat(graphs): add general Graph class

---
+++ src/graphs/Graphs.ts
@@ -0,0 +1,13 @@
+import { cubeDance } from "./cube-dance";
+import { octaTower } from "./octa-towers";
+import { powerTowers } from "./power-towers";
+import { weitzmannRegions } from "./weitzmann-regions";
+import { boretzRegions } from "./boretz-regions";
+
+export class Graphs {
+  static cubeDance = cubeDance;
+  static octaTower = octaTower;
+  static powerTowers = powerTowers;
+  static weitzmannRegions = weitzmannRegions;
+  static boretzRegions = boretzRegions;
+}
